aa3171a5714cdbdc1308574e38c4763155c4d8dd
Harrison <harrisonmetz17@gmail.com>
2026-03-04 16:15:52 -0900

    restructured code, added --verbose option and created calculator test app directory

--- main.py
+++ main.py
@@ -4,29 +4,32 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-parser = argparse.ArgumentParser(description="guntercli")
-parser.add_argument("user_prompt", type=str, help="User prompt")
-args = parser.parse_args()
-
-messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-if api_key == None:
-    raise RuntimeError("api key not found")
-client = genai.Client(api_key=api_key)
-response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
-metadata = response.usage_metadata
-if metadata != None:
-    print(f"Prompt tokens: {metadata.prompt_token_count}")
-    print(f"Response tokens: {metadata.candidates_token_count}")
-else:
-    raise RuntimeError("failed api request")
-
-print(f"Response: {response.text}")
 
 def main():
-    print("Hello from guntercli!")
+    parser = argparse.ArgumentParser(description="guntercli")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key == None:
+        raise RuntimeError("api key not found")
+    client = genai.Client(api_key=api_key)
+    
+
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+    metadata = response.usage_metadata
+    if metadata != None:
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {metadata.prompt_token_count}")
+            print(f"Response tokens: {metadata.candidates_token_count}")
+        print(f"Response: {response.text}")
+    else:
+        raise RuntimeError("failed api request")
+
+
 
 
 if __name__ == "__main__":
