b0a36d729719caafe04108021c0c4780915386ae
Harrison <harrisonmetz17@gmail.com>
2026-03-04 15:22:27 -0900

    added command line parsing and message history list

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
+parser = argparse.ArgumentParser(description="guntercli")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 if api_key == None:
     raise RuntimeError("api key not found")
 client = genai.Client(api_key=api_key)
-response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
 metadata = response.usage_metadata
 if metadata != None:
     print(f"Prompt tokens: {metadata.prompt_token_count}")
